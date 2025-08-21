baca54ed734ab3278164d8321e6fda5c59d4b543
筱序二十 <2907713872@qq.com>
2025-08-21 08:51:32 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -82,13 +82,13 @@
           >UptimeRobot</a> 接口 | 检测频率 5 分钟
         </div>
         <div>
-          Copyright © 2020 - {{ new Date().getFullYear() }} <a 
+          Copyright © 2024 - {{ new Date().getFullYear() }} <a 
             :href="pkg.repository.url"
             target="_blank"
             rel="noopener noreferrer"
             class="font-semibold hover:text-emerald-500 dark:hover:text-emerald-400 transition-colors"
           >
-            JLinmr
+            筱序二十
           </a>
         </div>
       </div>
